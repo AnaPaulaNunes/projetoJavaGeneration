63c44c43d860aa359ccfcfe1f9601baf3780d1c8
Ana Paula Nunes <112401017+AnaPaulaNunes@users.noreply.github.com>
2022-09-28 14:03:55 -0300

    Update Alunos.java

--- Alunos.java
+++ Alunos.java
@@ -58,16 +58,48 @@ public class Alunos extends Escola {
 		System.out.println("Qual a média do(a) "+getNome()+" no 4º Bimestre? ");
 		n4 = leia.nextFloat();
 		mediaFinal = (n1+n2+n3+n4)/4;
-		System.out.println("Média Final: "+mediaFinal);
+		System.out.println("\n===== Média Final: " + mediaFinal + " =====");
 		
 	}
-	
-	public void imprimirAluno() {
-		System.out.println("\n****************Aluno****************");
-		System.out.println("\nNome: "+getNome()+"\nGênero: "+getGenero()+"\nEndereço: "+getEndereco()+"\nCPF: "+getCpf()+"\nIdade: "
-				+getIdade()+"\nTelefone: "+getTelefone()+"\nRA: "+id+"\n"+serie+" série "+turma);
-		System.out.println("Média Final: "+mediaFinal);
+	public void leiaFaltas() {
+		int f1, f2, f3, f4;
+		System.out.println("Quantas faltas " + getNome() + " teve no 1º Bimestre? ");
+		f1 = leia.nextInt();
+		System.out.println("Quantas faltas " + getNome() + " teve no 2º Bimestre? ");
+		f2 = leia.nextInt();
+		System.out.println("Quantas faltas " + getNome() + " teve no 3º Bimestre? ");
+		f3 = leia.nextInt();
+		System.out.println("Quantas faltas " + getNome() + " teve no 4º Bimestre? ");
+		f4 = leia.nextInt();		totalFaltas = f1 + f2 + f3 + f4;
+		System.out.println("\n===== Total de faltas: " + totalFaltas + " =====");
 
 	}
 
+	public void aprovaAluno(){
+	if(mediaFinal >=7 && mediaFinal <=10){
+		System.out.println("\nAluna(o) APROVADA(O)!");
+	}else if(mediaFinal >=5 && mediaFinal <7){
+		System.out.println("\nAluna(o) de EXAME!\nEntre em contato com o seu professor!");
+	}else {
+		System.out.println("\nAluna(o) REPROVADA(O)!");
+		}
+	}
+
+	public void faltasAluno(){
+	if(totalFaltas >=0 && totalFaltas <=32){
+		System.out.println("\nAluna(o) dentro do limite de faltas permitido pela instituição!");
+	}else {
+		System.out.println("\nAluna(o) REPROVADA(O) por faltas!");
+		}
+	}
+
+	public void imprimirAluno() {
+		System.out.println("\n****************Aluno****************");
+		System.out.println("\nNome: " + getNome() + "\nGênero: " + getGenero() + "\nEndereço: " + getEndereco()
+				+ "\nCPF: " + getCpf() + "\nIdade: " + getIdade() + "\nTelefone: " + getTelefone() + "\n\nRA: " + id
+				+ "\n" + serie + " série " + turma);
+		System.out.println("Média Final: " + mediaFinal);
+		System.out.println("Total de faltas: " + totalFaltas);
+
+	}
 }
